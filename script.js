ec66c150e8eff7d93908ffeca1f7f8abdd582e41
raulprez <usernamerpc@gmail.com>
2022-05-27 08:44:03 -0700

    Add html skeleton

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateTextInput(val) {
+    var value1 = document.getElementById("range").value;
+    document.getElementById('value1').innerHTML =  `Grid: ${val}x${val}`;
+    return value1;
+  }
+
