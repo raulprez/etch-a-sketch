1043198957e3ec62d5241037d04e2873f000b2eb
raulprez <usernamerpc@gmail.com>
2022-05-30 13:09:13 -0700

    Add dynamic grid based on input value

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ function updateGrid(val) {
 
     resetGrid();
 
-    for (let i=0; i<val; i++) {
+    let grid = document.getElementById("etch-a-sketch");
+    grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
+
+    for (let i=0; i<val ** 2; i++) {
 
         let newGrid = document.createElement("div");
         newGrid.className = "newGrid";
