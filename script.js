add3128f8e47d207d316468495f55787701232d3
raulprez <usernamerpc@gmail.com>
2022-09-16 13:54:16 -0500

    Starter value of grid 16

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    updateGrid(16);
+});
+
 function updateTextInput(val) {
     let value = document.getElementById("range").value;
     document.getElementById('value').innerHTML =  `Grid: ${val}x${val}`;
@@ -11,18 +15,24 @@ function updateGrid(val) {
     let grid = document.getElementById("etch-a-sketch");
     grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
+    
 
     for (let i=0; i<val ** 2; i++) {
 
         let newGrid = document.createElement("div");
         newGrid.className = "newGrid";
+        newGrid.setAttribute("id", "newGrid");
         let grid = document.getElementById("etch-a-sketch");
         grid.appendChild(newGrid);
+
+        newGrid.addEventListener("mouseover", function (event) {
+        newGrid.style.backgroundColor = "red";
+        });
+
         console.log(val);
 
     }
 }
-
 
 function resetGrid() {
     grid = document.getElementById("etch-a-sketch");
