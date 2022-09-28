e5ae6868b438957d1a75619850df2f64f7665605
raulprez <usernamerpc@gmail.com>
2022-09-27 20:02:42 -0500

    Refactored code and commented functions for future use

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
+// Allows for the grids to be initialized from the moment DOM content is loaded
+
 document.addEventListener('DOMContentLoaded', (event) => {
     updateGrid(16);
 });
 
+// Updates the grid amounts displayed in numbers in HTML
+
 function updateTextInput(val) {
-    let value = document.getElementById("range").value;
     document.getElementById('value').innerHTML =  `Grid: ${val}x${val}`;
-    return value;
   };
+
+// Empty array initialized to store current color toggled
+
+  currentColor = [];  
+
+// Event listeners for buttons
 
 const button = document.getElementsByClassName("btn");
 
-// We have to addEventListener on every element
+// Study and understand why this works
+
 Array.prototype.map.call(button, (b) => {
     b.addEventListener("click", function(b) {
+        
+        // Uses the value from the targeted button and displays the current color in HTML
         let newColor = b.target.value;
         document.getElementById("currentColor").innerHTML = newColor;
-        console.log(newColor);
-        updateColor(newColor);
-        return newColor;
+        
+        // Pushes new color in empty array containing current color
+        currentColor.pop();
+        currentColor.push(newColor); 
     })
   })
-
-currentColor = ["black"];
-
-
-function updateColor (newColor) {
-    currentColor.push(newColor);
-    console.log(newColor);
-}
-
 
 function updateGrid(val) {
 
     resetGrid();
 
+    // Defines important parts of the grid wrapper and sets row and column size so it fits in a grid properly
     let grid = document.getElementById("etch-a-sketch");
     grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
     
 
+    // For loop that creates the proper amount of grid squares
     for (let i=0; i<val ** 2; i++) {
 
+        // Grid is the grid wrapper, new grid are the ones generated dynamically into the DOM
         let newGrid = document.createElement("div");
         newGrid.className = "newGrid";
         newGrid.setAttribute("id", "newGrid");
         let grid = document.getElementById("etch-a-sketch");
         grid.appendChild(newGrid);
 
+        // Mouseover event listener that creates the etch a sketch functionality, using the color stored in the array
         newGrid.addEventListener("mouseover", function (event) {
-        newGrid.style.backgroundColor = currentColor[currentColor.length-1];
-
+        newGrid.style.backgroundColor = currentColor;
         });
-
-        console.log(val);
-
     }
 }
 
