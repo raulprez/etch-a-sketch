efe03d33e806e9ea42b5f4bac6405d8dc23422de
raulprez <usernamerpc@gmail.com>
2022-11-12 20:10:21 -0600

    Finish shader

--- script.js
+++ script.js
@@ -21,20 +21,61 @@ const button = document.getElementsByClassName("btn");
 // Study and understand why this works
 
 let rainbow = false;
+let shader = false;
 
 Array.prototype.map.call(button, (b) => {
+
     b.addEventListener("click", function(b) {
 
       if (b.target.id == 'rainbow') {
         rainbow = true;
+        shader = false;
 
         var rainbowInterval = setInterval(function() {random_rgba(); }, 1);
           console.log(rainbow)
 
-      } else if (b.target.value != 'rainbow') {
+      } 
+      
+      else if (b.target.id == 'shader') {
+        shader = true;
         rainbow = false;
+        console.log(shader);
+
+        if (shader == true) {
+
+          const r = 255;
+          const b = 255;
+          const g = 255;
+
+          let shader = "rgb(" + r + "," + g + "," + b + ")";
+          currentColor.pop();
+          currentColor.push(shader); 
+
+          grid.onmouseover = function(){
+
+            console.log('shader working');
+            let shaderColor = "rgb(" + (r-20) + "," + (g-20) + "," + (b-20) + ")";
+            console.log(shaderColor);
+            currentColor.pop();
+            currentColor.push(shaderColor); 
+
+          };
+
+            
+
+
+        }
+
+        
+      }
+      
+      else if (b.target.value != 'rainbow') {
+
+        rainbow = false;
+        shader = false;
         clearInterval(rainbowInterval); 
         console.log(rainbow)
+
                 // Uses the value from the targeted button and displays the current color in HTML
                 let newColor = b.target.value;
                 document.getElementById("currentColor").innerHTML = newColor;
@@ -86,18 +127,8 @@ function random_rgba() {
   if (rainbow == true) {
     currentColor.pop();
     currentColor.push(randomRGB); 
-  } else {
-    return
-  }
-
+  } 
 }
-
-
-
-// https://stackoverflow.com/questions/17636407/javascript-changing-a-divs-background-color-to-a-random-color-on-mouseover
-
-
-
 
 // Fade color function
 
